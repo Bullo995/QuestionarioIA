6cd287fc76ffd58024152c561abb8393e0e92630
bullo995 <nicolabullita@gmail.com>
2024-04-14 22:06:40 +0200

    creazione domande dinamica

---
+++ model/domande.js
@@ -0,0 +1,136 @@
+const domande = [
+    {
+      id: 'data_nascita',
+      label: 'Data di nascita:',
+      type: 'date'
+    },
+    {
+      id: 'luogo_residenza',
+      label: 'Luogo di residenza:',
+      type: 'select',
+      options: [
+        'Abruzzo', 'Basilicata', 'Calabria', 'Campania', 'Emilia-Romagna', 'Friuli-Venezia Giulia',
+        'Lazio', 'Liguria', 'Lombardia', 'Marche', 'Molise', 'Piemonte', 'Puglia', 'Sardegna', 'Sicilia',
+        'Toscana', 'Trentino-Alto Adige', 'Umbria', "Valle d'Aosta", 'Veneto'
+      ]
+    },
+    {
+      id: 'familiarita_ia',
+      label: 'Quanto sei familiare con il concetto di Intelligenza Artificiale (IA)?',
+      type: 'select',
+      options: ['Molto familiare', 'Abbastanza familiare', 'Poco familiare', 'Non familiare']
+    },
+    {
+      id: 'opinione_ia',
+      label: 'Qual è la tua opinione generale sull\'IA?',
+      type: 'select',
+      options: ['Molto positiva', 'Positiva', 'Negativa', 'Molto negativa']
+    },
+    {
+      id: 'impatto_futuro_ia',
+      label: 'Credi che l\'IA abbia un impatto significativo sul futuro della società?',
+      type: 'select',
+      options: ['Molto impattante', 'Abbastanza impattante', 'Poco impattante', 'Non impattante']
+    },
+    {
+      id: 'impatto_futuro_ia_aperta',
+      label: 'Se la risposta è sì, per quali motivi? (Max 255 caratteri)',
+      type: 'textarea',
+      maxLength: 255,
+      disabled: true
+    },
+    {
+      id: 'paura_sostituzione_lavori',
+      label: 'Sei preoccupato che l\'IA possa sostituire alcuni lavori umani in futuro?',
+      type: 'select',
+      options: ['Molto preoccupato', 'Abbastanza preoccupato', 'Poco preoccupato', 'Non preoccupato']
+    },
+    {
+      id: 'paura_sostituzione_lavori_aperta',
+      label: 'Se la risposta è sì, per quali motivi? (Max 255 caratteri)',
+      type: 'textarea',
+      maxLength: 255,
+      disabled: true
+    },
+    {
+      id: 'settori_impatto',
+      label: 'Secondo te, quanto impatto avrà l\'intelligenza artificiale su settori specifici?',
+      type: 'select',
+      options: ['Molto impattante', 'Abbastanza impattante', 'Poco impattante', 'Non impattante']
+    },
+    {
+      id: 'settori_impatto_aperta',
+      label: 'Se pensi abbiano un impatto, quali pensi siano i settori più coinvolti? (Max 255 caratteri)',
+      type: 'textarea',
+      maxLength: 255,
+      disabled: true
+    },
+    {
+      id: 'preoccupazione_manipolazione',
+      label: 'Ti preoccupa la possibilità che l\'IA possa essere utilizzata per manipolare le persone o violare la loro privacy?',
+      type: 'select',
+      options: ['Molto preoccupato', 'Abbastanza preoccupato', 'Poco preoccupato', 'Non preoccupato']
+    },
+    {
+      id: 'preoccupazione_manipolazione_aperta',
+      label: 'Se la risposta è sì, per quali motivi? (Max 255 caratteri)',
+      type: 'textarea',
+      maxLength: 255,
+      disabled: true
+    },
+    {
+      id: 'contributo_risolvere_problemi',
+      label: 'Quanto credi che l\'IA possa contribuire a risolvere problemi globali, come la povertà o il cambiamento climatico?',
+      type: 'select',
+      options: ['Molto', 'Abbastanza', 'Poco', 'Niente']
+    },
+    {
+      id: 'contributo_risolvere_problemi_aperta',
+      label: 'Se pensi che contribuisca, per quali motivi? (Max 255 caratteri)',
+      type: 'textarea',
+      maxLength: 255,
+      disabled: true
+    },
+    {
+      id: 'ottimismo_pessimismo',
+      label: 'Quanto ti senti ottimista o pessimista riguardo al futuro in cui l\'IA diventerà sempre più diffusa?',
+      type: 'select',
+      options: ['Molto ottimista', 'Ottimista', 'Pessimista', 'Molto pessimista']
+    },
+    {
+      id: 'ottimismo_pessimismo_aperta',
+      label: 'Vuoi motivare la tua risposta? (Max 255 caratteri)',
+      type: 'textarea',
+      maxLength: 255,
+      disabled: true
+    },
+    {
+      id: 'istruzione_scolastica',
+      label: 'Quanto l\'istruzione sul tema dell\'IA, pensi debba essere parte del curriculum scolastico?',
+      type: 'select',
+      options: ['Molto presente', 'Abbastanza presente', 'Poco presente', 'Non presente']
+    },
+    {
+      id: 'istruzione_scolastica_aperta',
+      label: 'Se pensi debba essere presente, per quali motivi? (Max 255 caratteri)',
+      type: 'textarea',
+      maxLength: 255,
+      disabled: true
+    },
+    {
+      id: 'esperienza_utilizzo_ia',
+      label: 'Quanto hai utilizzato personalmente un sistema basato sull\'IA (come assistenti vocali, algoritmi di raccomandazione, etc.)?',
+      type: 'select',
+      options: ['Molto', 'Abbastanza', 'Poco', 'Niente']
+    },
+    {
+      id: 'esperienza_utilizzo_ia_aperta',
+      label: 'Se utilizzato, quale è stata la tua esperienza? (Max 255 caratteri)',
+      type: 'textarea',
+      maxLength: 255,
+      disabled: true
+    }
+  ];
+  
+
+module.exports = domande;
--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controller/controller');
+const domandeModel = require("../model/domande")
 
 router.get('/', (req, res) => {
     const errors = req.query.errori ? JSON.parse(req.query.errori) : null;
     const config = req.query.valori ? JSON.parse(req.query.valori) : null;
     const success = req.query.successo ? true : false;
-
-    res.render('index',{ errori: errors, valori: config, successo:success});
+    res.render('index',{ domande:domandeModel, errori: errors, valori: config, successo:success });
 });
 
 router.post('/invio', controller.salvaQuestionario);
